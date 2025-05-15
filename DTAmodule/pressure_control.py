5d159859ee9795d803d14a16e4acb439d89a912b
keisuke nagasawa <keisuke.nagasawa@example.com>
2025-05-15 14:50:48 +0900

    Update pressure controller with K2000 protocol

--- DTAmodule/pressure_control.py
+++ DTAmodule/pressure_control.py
@@ -16,7 +16,11 @@ class PressureController:
     def connect(self):
         """シリアル接続を確立"""
         try:
-            self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+            self.ser = serial.Serial(
+                port=self.port,
+                baudrate=self.baudrate,
+                timeout=2
+            )
             return True
         except Exception as e:
             print("接続エラー: {}".format(e))
@@ -38,7 +42,7 @@ class PressureController:
                 return None
         
         try:
-            self.ser.write("{}\r\n".format(command).encode())
+            self.ser.write((command + "\n").encode())
             time.sleep(0.1)
             response = self.ser.readline().decode().strip()
             return response
@@ -50,22 +54,69 @@ class PressureController:
         """現在の圧力を取得
         
         Returns:
-            float: 測定された圧力値
+            float: 測定された圧力値 (MPa)
         """
-        response = self.send_command("PV?")
         try:
-            return float(response)
-        except:
-            return 0.0
+            # リセット
+            self.send_command("*RST")
+            
+            # DC電圧測定モードに設定
+            self.send_command("FUNC 'VOLT:DC'")
+            
+            # ノイズ低減設定
+            self.send_command("VOLT:DC:NPLC 10")
+            
+            # 測定範囲設定
+            self.send_command("VOLT:DC:RANG 0.1")
+            
+            # 継続測定開始
+            self.send_command("INIT:CONT ON")
+            
+            # 設定が反映されるのを待つ
+            time.sleep(0.5)
+            
+            # データ取得
+            response = self.send_command("FETC?")
+            
+            if response:
+                # 電圧値を圧力に変換
+                voltage = float(response)
+                pressure = (voltage - 0.00001) * 133400 / 5.12285 + 0.1
+                return pressure
+            return None
+        except Exception as e:
+            print("圧力取得エラー: {}".format(e))
+            return None
     
     def set_pressure(self, pressure):
         """目標圧力を設定
         
         Args:
-            pressure (float): 設定する圧力値
+            pressure (float): 設定する圧力値 (MPa)
         """
-        command = "SV{}".format(pressure)
-        return self.send_command(command)
+        try:
+            # 圧力値を電圧に変換
+            voltage = ((pressure - 0.1) * 5.12285 / 133400) + 0.00001
+            
+            # リセット
+            self.send_command("*RST")
+            
+            # DC電圧出力モードに設定
+            self.send_command("FUNC 'VOLT:DC'")
+            
+            # 出力範囲設定
+            self.send_command("VOLT:DC:RANG 0.1")
+            
+            # 電圧を設定
+            self.send_command("VOLT:DC {}".format(voltage))
+            
+            # 出力開始
+            self.send_command("OUTP ON")
+            
+            return True
+        except Exception as e:
+            print("圧力設定エラー: {}".format(e))
+            return False
 
 # グローバルインスタンス
 pressure = PressureController()
@@ -74,7 +125,7 @@ def getPressure():
     """圧力を取得
     
     Returns:
-        float: 測定された圧力値
+        float: 測定された圧力値 (MPa)
     """
     return pressure.get_pressure()
 
@@ -82,6 +133,6 @@ def setPressure(pressure_value):
     """目標圧力を設定
     
     Args:
-        pressure_value (float): 設定する圧力値
+        pressure_value (float): 設定する圧力値 (MPa)
     """
     return pressure.set_pressure(pressure_value) 
