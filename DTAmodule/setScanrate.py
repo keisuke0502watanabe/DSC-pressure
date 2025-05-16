a1f573ab0ddafa09334dcc52e5101a26a9e5cbb2
keisuke nagasawa <keisuke.nagasawa@example.com>
2025-05-16 16:16:56 +0900

    Refactor: Use ChinoController in setScanrate.py

--- DTAmodule/setScanrate.py
+++ DTAmodule/setScanrate.py
@@ -1,17 +1,42 @@
+from DTAmodule.chino_control import ChinoController
 import time
-import Chino
+import datetime
+import threading
+import csv
+import os
+import vttotemp
+import am
+import traceback
+from natsort import natsorted
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pressure_control import PressureControl
+from DTAmodule.visualize import DTAVisualizer
+from DTAmodule.experiment_manager import ExperimentManager, ExperimentMetadata
+from collections import deque
+import keyboard
+import matplotlib.pyplot as plt
 
 def rate(Ti,Tf,rate):
-    t0 = time.time()
+    """温度を一定の速度で変化させる
+    
+    Args:
+        Ti (float): 開始温度
+        Tf (float): 終了温度
+        rate (float): 温度変化速度
+    """
+    chino = ChinoController()
+    chino.connect()
+    sv = Ti
     num = 0
-    while True:
-        c = num/60
-        sv = Ti+c
-        t1 = time.time()
-        t2 = t1-t0
+    t1 = time.time()
+    t2 = 0
+    while sv < Tf:
+        t2 = time.time() - t1
+        sv = Ti + rate * t2
         print(round(sv,3),round(t2,3))
         time.sleep(1)
-        Chino.setSv(sv)
+        chino.set_temperature(sv)
         num += rate
 #         mod = t2 % 60
 #         if (mod >= 30 and mod < 31) or (mod >= 0 and mod < 1):
